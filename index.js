e8fd005565d57eb117a9dd2468a6a04244710db6
Feyel-rnd <113343892+Feyel-rnd@users.noreply.github.com>
2022-09-14 16:08:30 +0200

    1 file modified

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // run `node index.js` in the terminal
 //'mongodb+srv://App:MyDbPassword@cluster0.fx8cutv.mongodb.net/?retryWrites=true&w=majority';
-const { MongoClient } = require("mongodb");
+const { MongoClient } = require('mongodb');
 
 // Connection URI
-const uri ='mongodb+srv://App:MyDbPassword@cluster0.fx8cutv.mongodb.net/?retryWrites=true&w=majority';
+const uri =
+  'mongodb+srv://App:MyDbPassword@cluster0.fx8cutv.mongodb.net/?retryWrites=true&w=majority';
 
 // Create a new MongoClient
 const client = new MongoClient(uri);
@@ -14,14 +15,13 @@ async function run() {
     await client.connect();
 
     // Establish and verify connection
-    await client.db("Data").command({ ping: 1 });
-    console.log("Connected successfully to server");
+    await client.db('Data').command({ ping: 1 });
+    console.log('Connected successfully to server');
   } finally {
     // Ensures that the client will close when you finish/error
     await client.close();
   }
+  console.log('I wanna log !');
 }
-//setInterval(run().catch(console.dir),2000)
 
-
-
+setInterval(run().catch(console.dir),2000)
