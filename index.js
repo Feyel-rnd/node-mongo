deca4a9683c34f4f4c039169325d8aa1a4d02296
Feyel-rnd <113343892+Feyel-rnd@users.noreply.github.com>
2022-09-14 13:50:52 +0200

    2 files modified, 1 added

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 // run `node index.js` in the terminal
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+//console.log(`Hello Node.js v${process.versions.node}!`);
+
+
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient;
+const uri = "mongodb+srv://App:MyDbPassword@cluster0.fx8cutv.mongodb.net/?retryWrites=true&w=majority"
+const mongo = new MongoClient(uri, {  useUnifiedTopology: true, connectTimeoutMS: 20000, useNewUrlParser: true});
+
+
+mongo.connect().then(err => {
+  const users = mongo.db("Data").collection("users");
+  users.insertOne({username: "my test"})
+console.log("done")
+
+})
+//uri : mongodb+srv://Owner:MyDbPassword@dbcoc-2ottt.gcp.mongodb.net/test?retryWrites=true&w=majority
+//real uri : mongodb+srv://App:MyDbPassword@cluster0.fx8cutv.mongodb.net/?retryWrites=true&w=majority
