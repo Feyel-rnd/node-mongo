206094be6e78591e71f6f394d03b8050f5b27956
Feyel-rnd <113343892+Feyel-rnd@users.noreply.github.com>
2022-09-15 11:17:29 +0200

    1 file modified

--- index.js
+++ index.js
@@ -3,32 +3,17 @@
 //const { MongoClient } = require('mongodb');
 //const uri ='mongodb://App:MyDbPassword@cluster0.fx8cutv.mongodb.net/?retryWrites=true&w=majority';
 //var MongoClient = require('mongodb').MongoClient;
+//const uri ='mongodb+srv://App:MyDbPassword@cluster0.fx8cutv.mongodb.net/?retryWrites=true&w=majority';
 
-
-
-
-
-
-  /**
-   * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-   * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-   */
-// Connection URI
-
-
-// Create a new MongoClient
-//const client = new MongoClient(uri);
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri ='mongodb://App:MyDbPassword@cluster0.fx8cutv.mongodb.net/?retryWrites=true&w=majority';
-const client = new MongoClient(uri);
+const uri = "mongodb+srv://App:MyDbPassword@cluster0.fx8cutv.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
-  const collection = client.db("data").collection("users")
-  console.log("Success !")
-  console.log(err)
-  //setTimeout(()=>{},2000)
-  collection.insertOne({name:"myname"})
-  //client.close();
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
+
 
